3a95db73a218bc072442f5e3e4ce7901f3981a70
Bree_Yogurt <33265027+tigertigeryoung@users.noreply.github.com>
2019-11-13 22:47:15 +0800

    Add files via upload

---
+++ image_stitching/main.py
@@ -0,0 +1,25 @@
+import cv2
+import sys
+import utils
+
+
+if __name__ == '__main__':
+
+    if sys.argv[1] and sys.argv[2]:
+
+        image1 = cv2.imread(sys.argv[1])
+        image2 = cv2.imread(sys.argv[2])
+        stitch_match = utils.FindKeyPointsAndMatching()
+        kp1, kp2 = stitch_match.get_key_points(img1=image1, img2=image2)
+        homo_matrix = stitch_match.match(kp1, kp2)
+        stitch_merge = utils.PasteTwoImages()
+        merge_image = stitch_merge(image1, image2, homo_matrix)
+        print('=======>Ready to show the stitched image!')
+        cv2.namedWindow('output', 0)
+        cv2.imshow('output', merge_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        cv2.imwrite('result_5&6.jpg', merge_image)
+        print('\n=======>Output saved!')
+    else:
+        print('input images location!')
